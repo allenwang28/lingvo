3df8d93969b4e60d9d8308344795c283eb45a7f4
Allen Wang <allencwang@google.com>
2021-10-26 16:25:51 -0500

    fix bucket

--- lingvo/tasks/lm/params/wiki_bert.py
+++ lingvo/tasks/lm/params/wiki_bert.py
@@ -148,12 +148,12 @@ class MLPerfTrainTemplate(BertTemplate):
     p.batch_size = self.BATCH_SIZE
     p.enable_packing = True
     p.shuffle = True
-    p.input_file = 'gs://mlperf_v1_1/bert/train-1024/*'
+    p.input_file = 'gs://allencwang-blinkfire/bert/train-1024/*'
     return p
 
   def Test(self):
     p = input_generator.TFRecordBertInput.Params()
-    p.input_file = 'gs://mlperf_v1_1/bert/eval/data'
+    p.input_file = 'gs://allencwang-blinkfire/bert/eval/data'
     p.name = 'test'
     p.batch_size = 512
     return p
