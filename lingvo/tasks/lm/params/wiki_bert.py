10f2aa584595d828f979a50538c0304aba91586e
Allen Wang <allencwang@google.com>
2021-10-28 13:36:54 -0500

    add transposed config

--- lingvo/tasks/lm/params/wiki_bert.py
+++ lingvo/tasks/lm/params/wiki_bert.py
@@ -385,6 +385,16 @@ class MLPerfBertDense13B32x32SingleStepTransposed(MLPerfBertDense13B32x32SingleS
 
 
 @model_registry.RegisterSingleTaskModel
+class MLPerfBertDense13B32x32SingleStepTransposed(MLPerfBertDense13B32x32SingleStep):
+  BATCH_SIZE = 2048
+  DEVICE_MESH_SHAPE = [256, 8]
+  DEVICE_MESH = np.reshape(
+      np.arange(0, np.product(DEVICE_MESH_SHAPE)), [32, 64]).transpose()
+  HIDDEN_DIM_RESHAPE_SEGMENTS = 8
+  MODEL_DIM_RESHAPE_SEGMENTS = [8]
+
+
+@model_registry.RegisterSingleTaskModel
 class MLPerfBertDense13B16x32SingleStep(MLPerfBertDense13B32x32SingleStep):
   DEVICE_MESH_SHAPE = [64, 16]
   device_mesh =  np.reshape(
