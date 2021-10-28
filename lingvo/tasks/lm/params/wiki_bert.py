4d85b6a4b04027f35749eaefd4707477e52034c5
Allen Wang <allencwang@google.com>
2021-10-28 12:06:26 -0500

    remove dupe

--- lingvo/tasks/lm/params/wiki_bert.py
+++ lingvo/tasks/lm/params/wiki_bert.py
@@ -72,7 +72,6 @@ class BertTemplate(base_model_params.SingleTaskModelParams):
         name='transformer',
         builder=builder_cls.Params().Set(
             atten_logit_cap=self.ATTEN_LOGIT_CAP,
-            attention_logits_dtype=tf.float32,
             attention_num_memory_heads=1,
             device_mesh_shape=self.DEVICE_MESH_SHAPE,
             device_mesh=self.DEVICE_MESH,
